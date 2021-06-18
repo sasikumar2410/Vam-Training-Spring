749253b56331c4cc4b625894a614916e3a69a13f
sasi chowdary <sasikumargadiparthi319@gmail.com>
2021-06-18 15:50:00 +0530

    PrintOnConsoleInputOnWedpage

--- SpringMvcWeb/src/main/java/com/vm/training/IndexController.java
+++ SpringMvcWeb/src/main/java/com/vm/training/IndexController.java
@@ -1,16 +1,27 @@
 package com.vm.training;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
 
-@RestController
-public class IndexController {
-	
-	
-	@RequestMapping("/")
-	public String Hello(){
-		return "hello  sasi kumar";
-	}
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index(
+        @RequestParam(value = "participant", required = false) String participant,
+        Model model
+    ) {
+    	
+       // model.addAttribute("id", id);
+        //List<Integer> userIds = Arrays.asList(1,2,3,4);
+       // model.addAttribute("userIds", userIds);
+        System.out.println(participant);
+        return "index";
+    }
+
 
 }
