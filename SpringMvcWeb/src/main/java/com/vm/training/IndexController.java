903f4c1672cd5677237bc757a4933c01db66c9f8
sasi chowdary <sasikumargadiparthi319@gmail.com>
2021-06-18 15:29:20 +0530

    SpringMvcWeb

---
+++ SpringMvcWeb/src/main/java/com/vm/training/IndexController.java
@@ -0,0 +1,16 @@
+package com.vm.training;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+	
+	
+	@RequestMapping("/")
+	public String Hello(){
+		return "hello  sasi kumar";
+	}
+
+}
