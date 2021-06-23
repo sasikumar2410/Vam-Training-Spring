ad3373173f90cb9101cf26922b846dd4a323796e
sasi chowdary <sasikumargadiparthi319@gmail.com>
2021-06-23 12:28:56 +0530

    Spring jUnit Testing

--- SpringTest-1/src/main/java/com/vm/training/EmployeeRepository.java
+++ SpringTest-1/src/main/java/com/vm/training/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package com.vm.training;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
